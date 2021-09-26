35b51e08fff285b0cd2b4e562ea56b2d3dfbad8f
BbChip0103 <bbchip13@gmail.com>
2021-09-26 14:19:46 +0000

    Update for upbit

--- collect_upbit_data.py
+++ collect_upbit_data.py
@@ -2,9 +2,12 @@ import asyncio
 import websockets
 import json
 import ccxt
+import os
+import os.path as pth
+import datetime as dt
 
 
-metadata_path = '../../metadata/coin_exchange.json'
+metadata_path = '../metadata/coin_exchange.json'
 with open(metadata_path) as f:
     exchange_api_dict = json.loads(f.read())
 
@@ -27,10 +30,27 @@ async def upbit_ws_client(coin_list, callback):
         subscribe_data = json.dumps(subscribe_fmt)
         await websocket.send(subscribe_data)
 
+        result_base_path = 'upbit'
+        os.makedirs(result_base_path, exist_ok=True)
+
         while True:
             res = await websocket.recv()
             res = json.loads(res)
-            print(res['cd'], res['tp'], res['tv'], res['ttms'])
+#            print(res['cd'], res['tp'], res['tv'], res['ttms'])
+            target_result = [res['ttms'], res['cd'], res['tp'], res['tv']]
+            target_result = list(map(str, target_result))
+
+            current_time = dt.datetime.now()
+            current_time_str = current_time.strftime('%Y%m%d_%H%M')
+            result_full_path = pth.join(result_base_path, current_time_str+'_'+'upbit.csv')
+            if not pth.isfile(result_full_path):
+                print(result_full_path)
+                with open(result_full_path, 'w') as f:
+                    f.write(','.join(['timestamp', 'symbol', 'price', 'volume'])+'\n')
+                    f.write(','.join(target_result)+'\n')
+            else:
+                with open(result_full_path, 'a') as f:
+                    f.write(','.join(target_result)+'\n')
 
 
 def get_upbit_coin_list():
@@ -78,9 +98,10 @@ def upbit_callback_func():
 
 if __name__ == "__main__":
     upbit_coin_list = get_upbit_coin_list()
-    binance_coin_list = get_binance_coin_list()
+#    binance_coin_list = get_binance_coin_list()
 
-    overlapped_coin_list = list(set(upbit_coin_list)&set(binance_coin_list)) 
+#    overlapped_coin_list = list(set(upbit_coin_list)&set(binance_coin_list)) 
+    overlapped_coin_list = upbit_coin_list
 
     tasks = [
         asyncio.ensure_future(
